8abc262945903a39b8dc486242819dc6578861a2
DjanoVanEijk <9028315@student.zadkine.nl>
2026-09-15 11:21:57 +0200

    opdracht 2 afgerond

--- week-02-herhaling/oefening-2/script.js
+++ week-02-herhaling/oefening-2/script.js
@@ -5,11 +5,28 @@
 // 1. Selecteer #title met querySelector en verander de tekst
 //    naar iets anders met textContent
 
+document.querySelector("#title").textContent = "Nieuwe tekst";
 
 // 2. Voeg een click event listener toe aan #toggle-btn.
 //    Bij klik wisselt de tekstkleur van #message tussen rood en zwart.
 //    Gebruik een variabele om bij te houden of de tekst nu rood is.
+function wisselkleur(){
+    if(document.querySelector("#message").style.color === "black"){
+    document.querySelector("#message").style.color = "red";
+    }
+    else{
+    document.querySelector("#message").style.color = "black";
+    }
+}
+
+document.querySelector("#toggle-btn").addEventListener("click", wisselkleur);
 
 
 // 3. Voeg een click event listener toe aan #greet-btn.
 //    Lees de waarde van #name-input uit en toon 'Hallo, <naam>!' in #greeting.
+
+function greet() {
+    const input = document.querySelector("#name-input").value;
+    document.querySelector("#greeting").textContent = "Hallo, " + input + "!";
+}
+document.querySelector("#greet-btn").addEventListener("click", greet);
